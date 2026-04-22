eb9c154a7254f40dcf4893c959e868eb6d996a81
Irfan Ahmed <110442688+ia22229@users.noreply.github.com>
2026-04-22 17:53:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://irfan2229.vercel.app',
-    'http://localhost:5000'
+    'http://localhost:3000'
   ]
 }));
 app.use(express.json());
