30b5da241cdb4b43e031e75abfb63f440a7c9e6e
Irfan Ahmed <110442688+ia22229@users.noreply.github.com>
2026-04-23 18:34:26 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -63,11 +63,17 @@ function Nav({ active, setActive, theme, toggleTheme }) {
       {/* Desktop-only: lock + theme shown in nav bar */}
       <div className="nav-desktop-icons">
         <button
-          className={active === 'Admin' ? 'active admin-btn' : 'admin-btn'}
-          onClick={() => setActive('Admin')}
-        >
-          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fillRule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clipRule="evenodd"/></svg>
-        </button>
+  className={active === 'Admin' ? 'active admin-btn' : 'admin-btn'}
+  onClick={() => setActive('Admin')}
+  title="Admin"
+  style={{ color: active === 'Admin' ? 'var(--accent)' : 'var(--text)' }}
+>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+    fill="currentColor" viewBox="0 0 24 24">
+    <path fillRule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clipRule="evenodd"/>
+  </svg>
+</button>
+        
         <button className="theme-toggle" onClick={toggleTheme} title="Toggle theme">
           {theme === 'dark' ? (
             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
